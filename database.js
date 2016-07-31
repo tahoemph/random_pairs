3f4e526326009a00ec27b8de0670c2d83974319f
Michael Hunter <mhunter@saymedia.com>
2016-07-30 21:55:49 -0700

    Use config.json for finding database

--- database.js
+++ database.js
@@ -2,10 +2,15 @@
 /*jslint node:true */
 "use strict";
 
+var fs = require('fs');
 var sequelize = require('sequelize');
 
+// Read the config syncronously.  This is a startup blocker so doing this async
+// doesn't help us much.
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
 var findDatabase = function() {
-    var database = new sequelize('sqlite://pairs.sqlite');
+    var database = new sequelize(config.database);
     return database;
 };
 
