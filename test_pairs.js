a2e69f1a15ed93896c0352c9603e66cdead943f4
Michael Hunter <mhunter@saymedia.com>
2016-08-10 21:17:56 -0700

    First implementation of test_pairs

--- test_pairs.js
+++ test_pairs.js
@@ -1,2 +1,17 @@
 // Checks that the CSV offered on the command line is unique.
+/*jshint node: true */
 "use strict";
+
+var csv = require('fast-csv');
+var database = require('./database');
+
+var db = database.findDatabase();
+var data = csv.fromPath('pairs.csv', {headers: true}).
+on("data", function(datum) {
+    var pair = {name1: datum.Name, name2: datum.Buddy};
+    database.readPair(pair).then(function(rows) {
+        if (rows.length !== 0) {
+            console.log(pair);
+        }
+    });
+});
